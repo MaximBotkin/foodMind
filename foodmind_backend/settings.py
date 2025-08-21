265e3b2cad5fbb96cfd171fc99bc566ae99b60ba
Botkin Maxim <68293738+MaximBotkin@users.noreply.github.com>
2025-08-21 21:11:48 +0300

    2

--- foodmind_backend/settings.py
+++ foodmind_backend/settings.py
@@ -90,15 +90,10 @@ WSGI_APPLICATION = 'foodmind_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
